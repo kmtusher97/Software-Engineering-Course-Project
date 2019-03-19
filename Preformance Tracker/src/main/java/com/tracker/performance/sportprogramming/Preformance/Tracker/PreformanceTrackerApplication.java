fb393ef4a552b372b67e7c0220292f0c9e23a83f
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2019-03-20 03:19:12 +0600

    Open project in Spring Boot, Dependency injections

---
+++ Preformance Tracker/src/main/java/com/tracker/performance/sportprogramming/Preformance/Tracker/PreformanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.performance.sportprogramming.Preformance.Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreformanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreformanceTrackerApplication.class, args);
+	}
+
+}
---
+++ Preformance Tracker/src/test/java/com/tracker/performance/sportprogramming/Preformance/Tracker/PreformanceTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tracker.performance.sportprogramming.Preformance.Tracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PreformanceTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
